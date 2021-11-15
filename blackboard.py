05775ca1068886ab40d94cc4e38f3840bd5ec029
yaonur <ya_onur@hotmail.com>
2021-11-15 21:26:01 +0300

    test

--- blackboard.py
+++ blackboard.py
@@ -6,10 +6,11 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.headless=True
 # options.binary_location = "/usr/lib/firefox/firefox"
-chrome_driver = ChromeDriverManager().install()
-driver = webdriver.Chrome(chrome_driver,options=options)
-# firefox_driver = GeckoDriverManager().install()
-# driver = webdriver.Firefox(options=options ,executable_path=firefox_driver)
+# chrome_driver = ChromeDriverManager().install()
+# driver = webdriver.Chrome(chrome_driver,options=options)
+firefox_driver = GeckoDriverManager().install()
+driver = webdriver.Firefox(options=options ,executable_path=firefox_driver)
 # driver=webdriver.Chrome(executable_path="/home/ynr/driver/chromedriver")
 # driver = webdriver.Firefox(executable_path="/home/ynr/driver/geckodriver", options=options)
 driver.get("https://www.google.com.tr/")
+print("finished")

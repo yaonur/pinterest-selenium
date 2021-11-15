098298301c2f8e6bc2619c96fd70c51247c8dd52
yaonur <ya_onur@hotmail.com>
2021-11-15 19:33:31 +0300

    test

--- blackboard.py
+++ blackboard.py
@@ -1,5 +1,15 @@
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
+options = Options()
+options.headless=True
+# options.binary_location = "/usr/lib/firefox/firefox"
 chrome_driver = ChromeDriverManager().install()
-driver=webdriver.Chrome(chrome_driver)
+driver = webdriver.Chrome(chrome_driver,options=options)
+# firefox_driver = GeckoDriverManager().install()
+# driver = webdriver.Firefox(options=options ,executable_path=firefox_driver)
+# driver=webdriver.Chrome(executable_path="/home/ynr/driver/chromedriver")
+# driver = webdriver.Firefox(executable_path="/home/ynr/driver/geckodriver", options=options)
+driver.get("https://www.google.com.tr/")
